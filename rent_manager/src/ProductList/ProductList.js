a85b61ad82bd6344af59c276e97ffb0faae0c573
Evelin Szabados <szabadosevelin7@gmail.com>
2020-04-15 13:48:23 +0200

    Add new button to product list items

---
+++ rent_manager/src/ProductList/ProductList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ProductListItems from "./ProductListItems";
+
+export default function ProductList() {
+  return (
+    <div>
+      <ProductListItems />
+    </div>
+  );
+}
