9397cce17499859476a7dd7daa8e825d5c9afc24
Evelin Szabados <szabadosevelin7@gmail.com>
2020-04-16 12:14:42 +0200

    Add data to material table

--- rent_manager/src/ProductList/ProductListItems.js
+++ rent_manager/src/ProductList/ProductListItems.js
@@ -1,89 +1,45 @@
-// import React, { useContext, useState } from "react";
-// import { makeStyles } from "@material-ui/core/styles";
-// import List from "@material-ui/core/List";
-// import ListItem from "@material-ui/core/ListItem";
-// import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-// import ListItemText from "@material-ui/core/ListItemText";
-import IconButton from "@material-ui/core/IconButton";
-import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
-// import EditIcon from "@material-ui/icons/Edit";
-// import { ProductContext } from "../context/ProductContext";
+import React, { useContext, useState } from "react";
+import { ProductContext } from "../context/ProductContext";
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     width: "100%",
-//     maxWidth: "70vw",
-//     backgroundColor: theme.palette.background.paper,
-//     "@media (max-width:600px)": {
-//       maxWidth: "100vw",
-//       margin: 0,
-//     },
-//   },
-// }));
-
-// export default function CheckboxList() {
-//   const { product } = useContext(ProductContext);
-//   const classes = useStyles();
-
-//   return (
-//     <List className={classes.root}>
-//       {product.map((value) => {
-//         const labelId = `checkbox-list-label-${value}`;
-
-//         return (
-//           <ListItem color={"primary"} key={value} role={undefined} dense button>
-//             <ListItemText id={labelId} primary={value.name} />
-//             <ListItemSecondaryAction>
-//               <ListItemText primary={value.price} />
-//             </ListItemSecondaryAction>
-//             <ListItemSecondaryAction>
-//               <ListItemText primary={value.statusName} />
-//             </ListItemSecondaryAction>
-//             <ListItemSecondaryAction>
-//               <IconButton edge="end" aria-label="edit">
-//                 <EditIcon />
-//               </IconButton>
-//               <IconButton edge="end" aria-label="delete">
-//                 <DeleteOutlineIcon />
-//               </IconButton>
-//             </ListItemSecondaryAction>
-//           </ListItem>
-//         );
-//       })}
-//     </List>
-//   );
-// }
-import React from "react";
 import MaterialTable from "material-table";
 
 export default function ProductListItems() {
-  const [state, setState] = React.useState({
+  const { product } = useContext(ProductContext);
+  const productDataContainer = [];
+  product.map((prod) => {
+    let data = {
+      name: prod.name,
+      statusName: prod.status_id,
+      price: prod.price,
+      categoryName: prod.category_id,
+    };
+    productDataContainer.push(data);
+  });
+  const [productData, setProductData] = useState(productDataContainer);
+
+  const [state, setState] = useState({
     columns: [
       { title: "Name", field: "name" },
-      { title: "Surname", field: "surname" },
-      { title: "Birth Year", field: "birthYear", type: "numeric" },
       {
-        title: "Birth Place",
-        field: "birthCity",
-        lookup: { 34: "İstanbul", 63: "Şanlıurfa" },
+        title: "Status",
+        field: "statusName",
+        lookup: { 1: "Available", 2: "Rented", 3: "Out of Operation" },
+      },
+      { title: "Price", field: "price", type: "numeric" },
+      {
+        title: "Category",
+        field: "categoryName",
+        lookup: { 1: "Betonkeverő", 2: "Betonsimító" },
       },
     ],
-    data: [
-      { name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63 },
-      {
-        name: "Zerya Betül",
-        surname: "Baran",
-        birthYear: 2017,
-        birthCity: 34,
-      },
-    ],
+    data: productDataContainer,
   });
 
   return (
     <MaterialTable
       title="Editable Example"
       columns={state.columns}
-      data={state.data}
+      data={productData}
       editable={{
         onRowAdd: (newData) =>
           new Promise((resolve) => {
