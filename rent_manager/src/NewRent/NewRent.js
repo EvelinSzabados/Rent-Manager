950c8ade1c6952920cd4dbc0b6e9d04dbc887537
Evelin Szabados <szabadosevelin7@gmail.com>
2020-04-28 10:26:41 +0200

    Save autocomplete value to context

--- rent_manager/src/NewRent/NewRent.js
+++ rent_manager/src/NewRent/NewRent.js
@@ -7,23 +7,48 @@ import CheckBoxIcon from '@material-ui/icons/CheckBox';
 import Button from "@material-ui/core/Button";
 import { ProductContext } from "../context/ProductContext";
 import { CustomerContext } from "../context/CustomerContext";
+import styled from "styled-components";
 
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
 const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
+const Date = styled.input`
+ 
+  width: 30vw;
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  @media (max-width: 600px) {
+    width: 80vw;
+  }
+`;
+const DateContainer = styled.div`
+  margin: 1rem;
+`;
 
 export default function NewRent() {
   const { product } = useContext(ProductContext);
   const { customer } = useContext(CustomerContext);
 
+  const [selectProduct, setSelectProduct] = useState();
+  const [selectCustomer, setSelectCustomer] = useState();
+
   function handleSubmit(event) {
+
+    const productSelect = document.querySelector('#product-select');
+    const customerSelect = document.querySelector('#customer-select');
+    const endDate = document.querySelector("#end-date");
+    const startDate = document.querySelector("#start-date");
     event.preventDefault();
-    console.log("submitted")
+    console.log(selectProduct);
+    console.log(selectCustomer)
+
   }
   return (
     <form onSubmit={handleSubmit}>
 
       <Autocomplete
+        onChange={(event, value) => setSelectProduct(value)}
         multiple
         id="product-select"
         options={product}
@@ -46,7 +71,7 @@ export default function NewRent() {
         )}
       />
       <Autocomplete
-
+        onChange={(event, value) => setSelectCustomer(value)}
         id="customer-select"
         options={customer}
         disableCloseOnSelect
@@ -61,13 +86,21 @@ export default function NewRent() {
           <TextField {...params} variant="outlined" label="Customers" placeholder="Select a customer" />
         )}
       />
-      <Button
-        label="Submit"
-        type="submit"
-        variant="contained"
-        color="primary"
-        style={{ margin: "1rem" }}
-      >
+      <div>
+        <DateContainer>
+          <p>Select start date:</p>
+          <Date id="start-date" type="date" />
+        </DateContainer>
+
+      </div>
+      <div>
+        <DateContainer>
+
+          <p>Select end date:</p>
+          <Date id="end-date" type="date" />
+        </DateContainer>
+      </div>
+      <Button label="Submit" type="submit" variant="contained" color="primary" style={{ margin: "1rem" }}>
         Submit
         </Button>
 
@@ -75,38 +108,3 @@ export default function NewRent() {
   );
 }
 
-// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
-const top100Films = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-  { title: 'The Dark Knight', year: 2008 },
-  { title: '12 Angry Men', year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: 'Pulp Fiction', year: 1994 },
-  { title: 'The Lord of the Rings: The Return of the King', year: 2003 },
-  { title: 'The Good, the Bad and the Ugly', year: 1966 },
-  { title: 'Fight Club', year: 1999 },
-  { title: 'The Lord of the Rings: The Fellowship of the Ring', year: 2001 },
-  { title: 'Star Wars: Episode V - The Empire Strikes Back', year: 1980 },
-  { title: 'Forrest Gump', year: 1994 },
-  { title: 'Inception', year: 2010 },
-  { title: 'The Lord of the Rings: The Two Towers', year: 2002 },
-  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { title: 'Goodfellas', year: 1990 },
-  { title: 'The Matrix', year: 1999 },
-  { title: 'Seven Samurai', year: 1954 },
-  { title: 'Star Wars: Episode IV - A New Hope', year: 1977 },
-  { title: 'City of God', year: 2002 },
-  { title: 'Se7en', year: 1995 },
-  { title: 'The Silence of the Lambs', year: 1991 },
-  { title: "It's a Wonderful Life", year: 1946 },
-  { title: 'Life Is Beautiful', year: 1997 },
-  { title: 'The Usual Suspects', year: 1995 },
-  { title: 'Léon: The Professional', year: 1994 },
-  { title: 'Spirited Away', year: 2001 },
-  { title: 'Saving Private Ryan', year: 1998 },
-  { title: 'Once Upon a Time in the West', year: 1968 },
-  { title: 'American History X', year: 1998 },
-  { title: 'Interstellar', year: 2014 },
-];
