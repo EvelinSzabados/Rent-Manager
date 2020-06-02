52380cd0b579ca8ba3ecfd6f3a2a779d4be14571
Evelin Szabados <szabadosevelin7@gmail.com>
2020-06-02 10:57:16 +0200

    Commit to merge

--- rent_manager/src/Login/PrivateRoute.js
+++ rent_manager/src/Login/PrivateRoute.js
@@ -4,7 +4,7 @@ import { Route, Redirect } from "react-router-dom";
 
 export const PrivateRoute = ({ component: Component, ...rest }) => {
     const { validLogin } = useContext(LoginContext);
-    console.log("Valid login from private route: " + validLogin)
+
     return (<Route {...rest} render={(props) => (
         validLogin === true
             ? <Component {...props} />
