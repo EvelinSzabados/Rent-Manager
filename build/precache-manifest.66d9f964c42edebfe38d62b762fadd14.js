0b8b8d7a79d089b2b2a67a4ee56b77ee0a7ac43b
Evelin Szabados <szabadosevelin7@gmail.com>
2020-06-15 11:54:23 +0200

    Add build dir

---
+++ build/precache-manifest.66d9f964c42edebfe38d62b762fadd14.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49906804ad6c845d7e054b422356cc32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5f4b4db2218bf4ce5c4",
+    "url": "/static/css/main.d69c410c.chunk.css"
+  },
+  {
+    "revision": "d2cf2fb85996fbc2fff8",
+    "url": "/static/js/2.6b90b662.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.6b90b662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f4b4db2218bf4ce5c4",
+    "url": "/static/js/main.e9853223.chunk.js"
+  },
+  {
+    "revision": "9fd19cebc9f50b5d1fd6",
+    "url": "/static/js/runtime-main.f1cc7120.js"
+  },
+  {
+    "revision": "4c5d5f975f46b83a5b87c7469189ef0d",
+    "url": "/static/media/logo.4c5d5f97.svg"
+  },
+  {
+    "revision": "573edc5cdab1524512468e81d0a6b8af",
+    "url": "/static/media/logocolored.573edc5c.svg"
+  }
+]);
